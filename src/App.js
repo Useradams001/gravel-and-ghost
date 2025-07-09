563998da35a04c2d982972278df1ecb86a990ea7
Useradams001 <167584765+Useradams001@users.noreply.github.com>
2025-07-09 12:06:15 -0600

    Add LandingPage component and update App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import './index.css';
+import LandingPage from './LandingPage';
 
 function App() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1 style={{ fontSize: '3rem' }}>Gravel & Ghost</h1>
-      <p>Some roads you ride to escape. Others ride with you.</p>
-      <button style={{ padding: '0.75rem 1.5rem', marginTop: '1rem' }}>Start Reading</button>
-    </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
